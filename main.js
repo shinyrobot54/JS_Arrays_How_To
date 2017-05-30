a67416b5119fdfd616e4f8be8f928b9124ac8fd7
Gabe Meyerson <Gabe Meyerson>
2017-05-30 17:35:29 -0400

    added some visuals

--- main.js
+++ main.js
@@ -1,54 +1,59 @@
-// //Single Dimensional Arrays
-// let fruits = ['Apple', 'Banana'];
-//
-// console.log(fruits);
-//
-// //Modifying SD Arrays
-// fruits[1] = 'Pear';
-//
-// console.log(fruits[1]);
-//
-// let newLength = fruits.push('Lemon');
-//
-// console.log(fruits);
-//
-// //Push, Pop, Shift, Unshift
-// let last = fruits.pop();
-//
-// console.log(fruits);
-//
-// let oldLength = fruits.unshift('Strawberry');
-//
-// console.log(fruits);
-//
-// let medLength = fruits.shift();
-//
-// console.log(fruits);
-//
-//
-// //Multi Dimensional Arrays
-// function mdArray (){
-//     let fruits;
-//     fruits = [['Pear', 'Soft', 'Sweet'], ['Apple', 'Hard', 'Tangy'], ['Lemon', 'Squishy', 'Sour']];
-//     console.log('\n' + fruits[0][0]);
-//     console.log(fruits[0][1]);
-//     console.log(fruits[0][2]);
-//     console.log('\n' + fruits[1][0]);
-//     console.log(fruits[1][1]);
-//     console.log(fruits[1][2]);
-//     console.log('\n' + fruits[2][0]);
-//     console.log(fruits[2][1]);
-//     console.log(fruits[2][2]);
-//
-// }
-// mdArray();
-//
-//
-// // Iterating over MD Arrays
-// let mdArray = [[5, 4, 3, 2, 1], [1, 2, 3, 4, 5], [0, 0, 0, 0, 0]];
-//
-// for (let i = 0; i < mdArray.length; i++) {
-//     for (let j = 0; j < mdArray[i].length; j++) {
-//         console.log('position: ' + i + ',' + j + '  = ' + mdArray[i][j]);
-//     }
-// }
+//Single Dimensional Arrays
+let fruits = ['Apple', 'Banana'];
+
+console.log(fruits);
+
+console.log('\n');
+console.log('\n');
+
+//Modifying SD Arrays
+fruits[1] = 'Pear';
+
+console.log(fruits[1]);
+
+let newLength = fruits.push('Lemon');
+
+console.log(fruits);
+
+console.log('\n');
+console.log('\n');
+
+//Push, Pop, Shift, Unshift
+let last = fruits.pop();
+
+console.log(fruits);
+
+let oldLength = fruits.unshift('Strawberry');
+
+console.log(fruits);
+
+let medLength = fruits.shift();
+
+console.log(fruits);
+
+console.log('\n');
+console.log('\n');
+
+//Multi Dimensional Arrays
+    fruits = [['Pear', 'Soft', 'Sweet'], ['Apple', 'Hard', 'Tangy'], ['Lemon', 'Squishy', 'Sour']];
+    console.log('\n' + fruits[0][0]);
+    console.log(fruits[0][1]);
+    console.log(fruits[0][2]);
+    console.log('\n' + fruits[1][0]);
+    console.log(fruits[1][1]);
+    console.log(fruits[1][2]);
+    console.log('\n' + fruits[2][0]);
+    console.log(fruits[2][1]);
+    console.log(fruits[2][2]);
+
+console.log('\n');
+console.log('\n');
+
+// Iterating over MD Arrays
+let mdArray = [[5, 4, 3, 2, 1], [1, 2, 3, 4, 5], [0, 0, 0, 0, 0]];
+
+for (let i = 0; i < mdArray.length; i++) {
+    for (let j = 0; j < mdArray[i].length; j++) {
+        console.log('position: ' + i + ',' + j + '  = ' + mdArray[i][j]);
+    }
+}
